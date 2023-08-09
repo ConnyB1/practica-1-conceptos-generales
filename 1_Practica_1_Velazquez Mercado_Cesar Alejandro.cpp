c016e8db4987e07a84d0f987442c42d7fe2fcc71
Alejandro Velazquez <alejandrovelazquezmercado21@gmail.com>
2023-08-09 15:32:46 -0700

    new file:   1_Practica_1_Velazquez Mercado_Cesar Alejandro.cpp 	new file:   1_Practica_1_Velazquez Mercado_Cesar Alejandro.exe

---
+++ 1_Practica_1_Velazquez Mercado_Cesar Alejandro.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int limite;
+    int resultado = 0;
+
+    printf("ingrese un número entero positivo");
+    scanf("%d",&limite);
+    for (int i=1; i<=limite; i++)
+    {
+        resultado +=i;
+    }
+    printf("La suma de los primeros %d números es: %d\n", limite, resultado);
+    return 0;
+}
